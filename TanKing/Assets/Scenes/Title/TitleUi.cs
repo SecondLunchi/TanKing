d6e9f29dd0a3375070a1b4a651d1ac7d488cb428
ニドベン <SecondLunchi20@gmail.com>
2020-09-20 19:21:24 +0900

    TileMenu

---
+++ TanKing/Assets/Scenes/Title/TitleUi.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class TitleUi : MonoBehaviour {
+
+    GameObject Title;
+    bool presstoanykey = true;
+
+    private void Start() {
+        //Tile表示
+        Title = GameObject.Find("Title");
+        Title.SetActive(false);
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.A) || presstoanykey) {
+            PressToAnyKey();
+            presstoanykey = false;
+        }
+    }
+
+    public void PressToAnyKey() {
+        Title.SetActive(true);
+        GameObject.Find("PressToAnyKey").SetActive(false);
+    }
+
+    public void SoccerStart() {
+        SceneManager.LoadScene("Soccer");
+    }
+
+
+
+}
